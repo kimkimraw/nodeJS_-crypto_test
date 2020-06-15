ea0c1edd399da6c0987f2d4060805f248e895bc2
sou kimura <nattoukimuti0201@gmail.com>
2020-06-15 21:50:54 +0900

    files commit

--- npx/app.js
+++ npx/app.js
@@ -1,12 +1,23 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// 暗号化モジュール
+let crypto = require("crypto");
 
-var app = express();
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+
+let app = express();
+
+// 暗号化アルゴリズム
+let cipers = crypto.getCiphers();
+console.log(cipers);
+
+// ハッシュアルゴリズム
+let hashes = crypto.getHashes();
+console.log(hashes);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +27,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
+
+
+
+var planeText = 'This is Plane Text';
+var passowrd = 'passw0rd';
+
+console.log('暗号化するテキスト : ' + planeText);
+console.log('暗号化キー        : ' + passowrd);
+
+// 暗号化
+var cipher = crypto.createCipher('aes192', passowrd);
+cipher.update(planeText, 'utf8', 'hex');
+var cipheredText = cipher.final('hex');
+
+console.log('暗号化(AES192) :');
+console.log(cipheredText);
+
+// 復号
+var decipher = crypto.createDecipher('aes192', passowrd);
+decipher.update(cipheredText, 'hex', 'utf8');
+var dec = decipher.final('utf8');
+
+console.log('復号化(AES192) : ');
+console.log(dec);
+
+
+
+
+
+
+
+
+
 
 module.exports = app;
